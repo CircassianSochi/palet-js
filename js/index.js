57cee719ca3115b7f80d546e4d34397e6891ad79
CircassianSochi <oguzhanylmz9695@hotmail.com>
2018-05-30 08:23:09 +0300

    final

---
+++ js/index.js
@@ -0,0 +1,4 @@
+//https://github.com/CircassianSochi/palet-js
+var colors = ["aquamarine", "pink", "darkturquoise", "steelblue", "cadetblue", "lightblue", "lightsalmon", "teal", "darkseagreen", "khaki", "lightcoral", "lightgray", "coral", "slategray", "gray", "lightseagreen"];
+
+palette(colors, document.querySelector('.example'));
